51046d25aecfd45378b90f153fd933dbb4b4136a
anfractuosity <anfractuosity@anfractuosity.com>
2020-01-25 20:43:55 +0000

    fix unicode error

--- FirefoxBookmarkExtractor.py
+++ FirefoxBookmarkExtractor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import operator
 import sqlite3
 import glob
 import csv
@@ -10,7 +11,7 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dir", help="Directory containing places.sqlite")
-parser.add_argument("download_dir", help="Directory to check for webpages and to write webpages to") 
+#parser.add_argument("download_dir", help="Directory to check for webpages and to write webpages to") 
 parser.add_argument("csv", help="Filename for CSV output")                                                                                                                             
 parser.parse_args()
 args = parser.parse_args()
@@ -24,25 +25,28 @@ for root, dirs, files in os.walk(args.dir,followlinks=True):
     for file in files:
         if file == "places.sqlite":
             places.append(os.path.join(root, file))
-print(places)
 
 for p in places:
+
     conn = sqlite3.connect(p)
     conn.row_factory = sqlite3.Row
+    conn.text_factory = lambda b: b.decode(errors = 'ignore')
     c = conn.cursor()
     c.execute('select * FROM moz_bookmarks AS a JOIN moz_places AS b ON a.fk = b.id')
 
     for r in c.fetchall():
-        placeshash[r["url"]] = r["title"]
+        placeshash[r["url"]] = (r["title"],r["dateAdded"])
 
 with open(args.csv, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+                            quotechar='"', quoting=csv.QUOTE_NONNUMERIC)    
 
-    for k,v in placeshash.items():
+    b = placeshash.items()
+
+    for k,v in sorted(b,key=lambda x:x[1][1]):
         h = hashlib.new('md5')
         h.update(k.encode("utf-8"))
-        csvwriter.writerow([h.hexdigest(),k,v])
+        csvwriter.writerow([h.hexdigest(),k,v[0],v[1]])
         
 """
         urlfolder = h.hexdigest()
